27b8179fe7e573dccd01ec75f819b16f81756567
Marco Boßmann <info@bossweb.eu>
2015-06-02 19:45:08 +0200

    Umsortierung typo3 Ordner + Typo3 Skeleton (Fileadmin/...)

---
+++ typo3/skeleton/fileadmin/templates/main/js/core.js
@@ -0,0 +1,12 @@
+/*
+ * @author Marco Boﬂmann
+ * @copyright 2015 BOSSWEB <info@bossweb.eu>
+ * 
+ * Wenn am Anfang alles schief geht, nenne es Version 1.0!
+ */
+
+// On DOM ready
+ 
+$(document).ready(function() {
+	 // Hier kommen dein Code hinein
+});
